039f40536a88cafd445fe82927aa6dcbc5a4ef0c
Nithin T <nithint4034@gmail.com>
2023-12-05 11:35:47 +0530

    Implementation of the capture design, longitude latitude

--- app/componenets/Cam.js
+++ app/componenets/Cam.js
@@ -1,77 +1,55 @@
-// // Camera.js
-
-// import React, { useEffect } from 'react';
-// import { View, Text, TouchableOpacity, StyleSheet, BackHandler } from 'react-native';
-// // import { Camera } from 'react-native-vision-camera';
-
-// const CameraApp = ({ navigation }) => {
-//   useEffect(() => {
-//     checkPermission()
-//     },[]);
-
-//     const checkPermission = async () => {
-//       const newCameraPermission = await Camera.getCameraPermissionStatus()
-//       console.log(newCameraPermission);
-//     }
-
-//   return (
-//     <View style={styles.container}>
-//       <Text>Camera</Text>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   button: {
-//     backgroundColor: '#1b1b33',
-//     padding: 10,
-//     borderRadius: 5,
-//     marginTop: 20, // Add spacing between text and button
-//   },
-//   buttonText: {
-//     color: 'white',
-//     fontSize: 16,
-//     textAlign: 'center',
-//   },
-// });
-
-// export default CameraApp;
-
 import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
+import * as Location from 'expo-location';
 import * as FileSystem from 'expo-file-system';
+import Toast from 'react-native-toast-message';
 
 export default function Cam() {
-  const [hasPermission, setHasPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [hasLocationPermission, setHasLocationPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const cameraRef = useRef(null);
 
   useEffect(() => {
     (async () => {
-      const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      const { status: cameraStatus } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(cameraStatus === 'granted');
+
+      const { status: locationStatus } = await Location.requestForegroundPermissionsAsync();
+      setHasLocationPermission(locationStatus === 'granted');
     })();
   }, []);
 
+  const showToast = (type, text1, text2) => {
+    Toast.show({
+      type: type,
+      text1: text1,
+      text2: text2,
+    });
+  };
+
   const handleTakePicture = async () => {
-    if (cameraRef.current) {
+    if (cameraRef.current && hasLocationPermission) {
       try {
+        // Fetch current location
+        const location = await Location.getCurrentPositionAsync({});
+        console.log('Location:', location.coords.latitude, location.coords.longitude);
+
+        // Take picture
         const { uri } = await cameraRef.current.takePictureAsync();
         console.log('Photo captured:', uri);
 
         // Convert image to base64
         const base64 = await convertToBase64(uri);
-        console.log('Base64 representation:', base64);
+        // console.log('Base64 representation:', base64);
 
-        // You can handle the captured photo URI and base64 string as needed
+        // Show success toast
+        showToast('success', 'Photo Captured', `Location: ${location.coords.latitude}, ${location.coords.longitude}`);
       } catch (error) {
         console.error('Error taking picture:', error);
+        // Show error toast
+        showToast('error', 'Error Taking Picture', 'Please try again');
       }
     }
   };
@@ -84,14 +62,15 @@ export default function Cam() {
       return base64;
     } catch (error) {
       console.error('Error converting to base64:', error);
+      throw error;
     }
   };
 
-  if (hasPermission === null) {
+  if (hasCameraPermission === null || hasLocationPermission === null) {
     return <View />;
   }
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  if (hasCameraPermission === false || hasLocationPermission === false) {
+    return <Text>No access to camera or location</Text>;
   }
 
   return (
@@ -116,21 +95,30 @@ export default function Cam() {
                   : Camera.Constants.Type.back
               );
             }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            {/* <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text> */}
           </TouchableOpacity>
           <TouchableOpacity
             style={{
-              flex: 0.1,
-              alignSelf: 'flex-end',
-              alignItems: 'center',
+              position: 'absolute',
+              bottom: 16,
+              right: 145, // Adjust the right position as needed
             }}
             onPress={handleTakePicture}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Capture </Text>
+            <View
+              style={{
+                width: 70,
+                height: 70,
+                backgroundColor: 'red',
+                borderRadius: 35,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <Text style={{ fontSize: 18, color: 'white' }}> Capture </Text>
+            </View>
           </TouchableOpacity>
         </View>
       </Camera>
+      <Toast ref={(ref) => Toast.setRef(ref)} />
     </View>
   );
 }
-
-

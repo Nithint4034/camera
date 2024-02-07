f6f1ad86434eb910b2447b5af073da39fad12a7c
Nithin T <nithint4034@gmail.com>
2024-02-07 12:58:16 +0530

    small changes

--- app/api/client.js
+++ app/api/client.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export default axios.create({ baseURL: 'https://dtravelshoot.deducetech.com' });  //Local
+export default axios.create({ baseURL: 'https://travelshoot.deducetech.com:9387' });  //Local
 // export default axios.create({ baseURL: 'http://10.10.5.130:8793' });  //Server
 
 
